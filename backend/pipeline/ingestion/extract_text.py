6b7bd88d3ba45bce36808831bcdb4107bf1586ff
ashutosh229 <akumarjha875@gmail.com>
2025-03-22 21:43:17 +0530

    added the text extracter

--- backend/pipeline/ingestion/extract_text.py
+++ backend/pipeline/ingestion/extract_text.py
@@ -8,6 +8,18 @@ def extract_text_from_pdf(pdf_path):
         text = text + page.get_text("text")+"\n"
     return text.strip()
 
+def extract_text_from_folder(path):
+    all_text = ""
+    pdf_files = sorted([f for f in os.listdir(path) if f.endswith(".pdf")])
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(path,pdf_file)
+        all_text = all_text + extract_text_from_pdf(pdf_path) + "\n\n"
+    return all_text.strip()
+
 if __name__=="__main__":
-    pdf_path = "../../data/books/class10_science.pdf"
-    extracted_text = extract_text_from_pdf(pdf_path)
+    folder_path = "../../data/books/class_10_science"
+    extracted_text = extract_text_from_folder(folder_path)
+    with open("../../data/books_text/class10_science_text.txt", "w", encoding="utf-8") as f:
+        f.write(extracted_text)
+    print(extracted_text[:500])
+    
